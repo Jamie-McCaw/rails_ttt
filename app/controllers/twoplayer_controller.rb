981e7f2aa7ebda4968f9bbddc7265d03550d12e1
CoryFoy <github@cornetdesign.com>
2013-01-02 16:47:23 -0500

    refactoring

--- app/controllers/twoplayer_controller.rb
+++ app/controllers/twoplayer_controller.rb
@@ -9,128 +9,70 @@ O_PLAYER = 'O'
 
 class TwoplayerController < ApplicationController
 	def home
-		tttgame = Tictactoe.new
-		tttgame.game = Game.new.board.cells 
-		tttgame.player = X_PLAYER
-		tttgame.save
-		set_session_ids(tttgame)
-		set_view_variables(tttgame)
+          tttgame = Tictactoe.create_x_game
+          render_game(tttgame)
 	end
 
 	def second_player
-		tttgame = Tictactoe.find(params['id'])
-		tttgame.player = O_PLAYER
-		tttgame.save
-		set_session_ids(tttgame)
-		set_view_variables(tttgame)
-		render :template => 'twoplayer/home'
+          tttgame = Tictactoe.create_y_game_for(params['id'])
+          render_game(tttgame)
 	end
 
-	def move
-		game, tttgame, player = move_setup
-		if not_playable?(game)
-			invalid_choice(game)
-		else
-			if player == tttgame.player
-				make_the_move(tttgame, game, player)
-			else
-				invalid_choice(game)
-			end
-		end
-	end
-
-	def not_playable?(game)
-		game.board.game_over? || !game.board.move_available?(params['cell'].to_i)
-	end
-
-	def move_setup
-		game = Game.new
-		tttgame = Tictactoe.find(session[:game_id])
-		player = session[:player_id]
-		game.board.cells = tttgame.game
-		return game, tttgame, player
-	end
-
-	def invalid_choice(game)
-		@move = 'invalid'
-		check_game_state(game)
-	end
-
-	def check_game_state(game)
-		if @move == 'invalid'
-			invalid_move(game)
-		elsif game.board.winner
-			game_over(game)
-		elsif game.board.tie?
-			stalemate(game)
-		else
-			make_move(game)
-		end
-	end
-
-	def make_the_move(tttgame, game, player)
-		place_move_on_board(game, player)
-		switch_player(tttgame)
-		save_game(tttgame, game)
-	end
-
-	def save_game(tttgame, game)
-		tttgame.game = game.board.cells
-		tttgame.save
-	end
-
-	def place_move_on_board(game, player)
-		game.board.move(params["cell"].to_i, player)
-		check_game_state(game)
-	end
-
-	def switch_player(tttgame)
-		tttgame.player = tttgame.player == X_PLAYER ? O_PLAYER : X_PLAYER
-	end
-
-	def board
-		game, tttgame, player = move_setup
-		turn = tttgame.player
-		game_state_data = get_game_state(game)
-		render :json => { :board => tttgame.game, :players_turn => turn, :state => game_state_data }
-	end
-
-	def get_game_state(game)
-		if game.board.winner
-			return game.board.winner
-		elsif game.board.tie?
-			return 'Tie'
-		else
-			return 'Continue the Game'
-		end
-	end
-
-	def game_over(game)
-		render :json => { :game_over => "Player #{game.board.winner} Wins!", :player => params[:cell], :type => session[:player_id] }
-	end
-
-	def stalemate(game)
-		render :json => { :stalemate => 'Tie Game.', :player => params[:cell], :type => session[:player_id] }
-	end
-
-	def make_move(game)
-		render :json => { :player => params[:cell], :type => session[:player_id] }
-	end
-
-	def invalid_move(game)
-		render :json => {:invalid => "Invalid Move"}
-	end
+        def render_game(tttgame)
+          set_session_ids(tttgame)
+          set_view_variables(tttgame)
+          render :home
+        end
 
 	def set_view_variables(tttgame)
-		@cells = tttgame.game
-		@host = request.host_with_port
-		@protocol = request.protocol
+          @cells = tttgame.game
+          @host = request.host_with_port
+          @protocol = request.protocol
 	end
 
 	def set_session_ids(tttgame)
-		@game_id = tttgame.id		
-		player_id = tttgame.player
-		session[:player_id] = player_id
-		session[:game_id] = @game_id
+          @game_id = tttgame.id
+          player_id = tttgame.player
+          session[:player_id] = player_id
+          session[:game_id] = @game_id
 	end
+
+	def move
+          game, result = TicTacToe.play_move_for(session[:game_id], session[:player_id], params[:cell].to_i)
+
+          case result
+          when 'invalid'
+            invalid_move(game)
+          when 'winner'
+            game_over(game)
+          when 'tie'
+            stalemate(game)
+          else
+            make_move(game)
+          end
+	end
+
+	def board
+          game, tttgame, player = Tictactoe.move_setup(session[:game_id], session[:player_id])
+          turn = tttgame.player
+          game_state_data = Tictactoe.get_game_state(game)
+          render :json => { :board => tttgame.game, :players_turn => turn, :state => game_state_data }
+	end
+
+	def game_over(game)
+          render :json => { :game_over => "Player #{game.board.winner} Wins!", :player => params[:cell], :type => session[:player_id] }
+	end
+
+	def stalemate(game)
+          render :json => { :stalemate => 'Tie Game.', :player => params[:cell], :type => session[:player_id] }
+	end
+
+	def make_move(game)
+          render :json => { :player => params[:cell], :type => session[:player_id] }
+	end
+
+	def invalid_move(game)
+          render :json => {:invalid => "Invalid Move"}
+	end
+
 end
--- app/models/tictactoe.rb
+++ app/models/tictactoe.rb
@@ -1,4 +1,96 @@
 class Tictactoe < ActiveRecord::Base
   attr_accessible :game
   serialize :game
+
+  def self.create_x_game
+    tttgame = Tictactoe.new
+    tttgame.game = Game.new.board.cells 
+    tttgame.player = X_PLAYER
+    tttgame.save!
+    tttgame
+  end
+
+  def self.create_y_game_for(id)
+    tttgame = Tictactoe.find(id)
+    tttgame.player = O_PLAYER
+    tttgame.save!
+    tttgame
+  end
+
+  def self.play_move_for(game_id, player_id, cell)
+    game = Game.new
+    tttgame = Tictactoe.find(game_id)
+    player = player_id
+    game.board.cells = tttgame.game
+
+    if not_playable?(game, cell)
+      invalid_choice(game)
+    else
+      if player == tttgame.player
+        make_the_move(tttgame, game, player, cell)
+      else
+        invalid_choice(game)
+      end
+    end
+  end
+
+  def self.move_setup(game_id, player_id)
+    game = Game.new
+    tttgame = Tictactoe.find(game_id)
+    player = player_id
+    game.board.cells = tttgame.game
+    return game, tttgame, player
+  end
+
+  def self.get_game_state(game)
+    if game.board.winner
+      return game.board.winner
+    elsif game.board.tie?
+      return 'Tie'
+    else
+      return 'Continue the Game'
+    end
+  end
+
+  def self.not_playable?(game, cell)
+    game.board.game_over? || !game.board.move_available?(cell)
+  end
+
+  def self.invalid_choice(game)
+    @move = 'invalid'
+    check_game_state(game)
+  end
+
+  def self.check_game_state(game)
+    if @move == 'invalid'
+      return game, 'invalid'
+    elsif game.board.winner
+      return game, 'winner'
+    elsif game.board.tie?
+      return game, 'tie'
+    else
+      return game, 'playon'
+    end
+  end
+
+  def self.make_the_move(tttgame, game, player, cell)
+    place_move_on_board(game, player, cell)
+    switch_player(tttgame)
+    save_game(tttgame, game)
+  end
+
+  def self.save_game(tttgame, game)
+    tttgame.game = game.board.cells
+    tttgame.save
+  end
+
+  def self.place_move_on_board(game, player, cell)
+    game.board.move(cell, player)
+    check_game_state(game)
+  end
+
+  def self.switch_player(tttgame)
+    tttgame.player = tttgame.player == X_PLAYER ? O_PLAYER : X_PLAYER
+  end
+
 end
